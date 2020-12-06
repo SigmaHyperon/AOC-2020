db341b5030cedefbeff799e0085407132801e0b1
SigmaHyperon <nano_tech@web.de>
2020-12-06 20:47:03 +0100

    improved 06

--- 06/index.js
+++ 06/index.js
@@ -4,18 +4,8 @@ const data = fs.readFileSync("input.txt");
 const values = data.toString().split("\n\n").map(parseLine);
 
 function parseLine(line) {
-	const people = [...line].filter(v => v === "\n").length + 1;
-	const answers = {};
-	for(let i = 0; i < line.length; i++) {
-		const char = line[i];
-		if(char.match(/\w/)) {
-			if(typeof answers[char] === "undefined") {
-				answers[char] = 1;
-			} else {
-				answers[char]++;
-			}
-		}
-	}
+	const people = line.split("\n").length;
+	const answers = [...(line.replace(/\n/g, ""))].reduce((acc,v) => (acc[v] = (acc[v] || 0) + 1, acc), {});
 	return {people, answers};
 }
 
@@ -23,7 +13,7 @@ function groupAnswerCount01(line) {
 	return Object.keys(line?.answers).length;
 }
 function groupAnswerCount02(line) {
-	return Object.entries(line?.answers).filter(v => v[1] === line?.people).length;
+	return Object.values(line?.answers).filter(v => v === line?.people).length;
 }
 
 console.log(`total answers: ${values.map(groupAnswerCount01).reduce((acc,v) => acc + v, 0)}`);
