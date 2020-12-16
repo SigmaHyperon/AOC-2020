b300e85705d7ec84f6e3d157362c8a75bc7a6763
SigmaHyperon <nano_tech@web.de>
2020-12-16 17:51:43 +0100

    solved 16

---
+++ 16/index.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("input.txt");
+const values = data.toString().split("\n\n");
+
+const rules = values[0].split("\n").map(parseRule);
+const myTicket = values[1].split("\n").slice(1).map(parseTicket)[0];
+const otherTickets = values[2].split("\n").filter(v => v.length > 0).slice(1).map(parseTicket);
+
+function parseRule(line) {
+	const parts = line.match(/(.+): (\d+)\-(\d+) or (\d+)\-(\d+)/);
+	return{name: parts[1], first: [parseInt(parts[2]), parseInt(parts[3])], second: [parseInt(parts[4]), parseInt(parts[5])]};
+}
+
+function parseTicket(line) {
+	return {values: line.split(",").map(v => parseInt(v))};
+}
+
+function getErrorRate(ticket, rules) {
+	return ticket.values.filter(v => getMatchingRules(v, rules).length === 0).reduce((acc, v) => acc + v, 0);
+}
+
+function getMatchingRules(value, rules) {
+	return rules.filter(v => (v.first[0] <= value && v.first[1] >= value) || (v.second[0] <= value && v.second[1] >= value)).map(v => v.name);
+}
+
+console.time("time");
+console.log("part1:", otherTickets.map(v => getErrorRate(v, rules)).reduce((acc, v) => acc + v, 0));
+console.timeEnd("time");
+
+function getValidTickets(tickets, rules) {
+	return tickets.filter(v => getErrorRate(v, rules) === 0);
+}
+
+function getFieldSequence(tickets, rules) {
+	let fields = new Map();
+	const vTickets = getValidTickets(tickets, rules);
+
+	for(let ticket of vTickets) {
+		for(const [index, value] of ticket.values.entries()) {
+			const mRules = getMatchingRules(value, rules);
+			if(!fields.has(index)) {
+				fields.set(index, mRules);
+			} else if(fields.get(index).length > 1) {
+				fields.set(index, fields.get(index).filter(v => mRules.includes(v)));
+			}
+		}
+	}
+
+	fields = Array.from(fields.entries()).sort((a, b) => a[1].length - b[1].length);
+
+	let final = new Map();
+	for(let field of fields) {
+		final.set(field[1].filter(v => !Array.from(final.keys()).includes(v))[0], field[0]);
+	}
+	return final;
+}
+
+console.time("time");
+console.log("part2:",Array.from(getFieldSequence(otherTickets, rules).entries()).filter(v => v[0].startsWith("departure")).map(v => myTicket.values[v[1]]).reduce((acc, v) => acc * v, 1));
+console.timeEnd("time");
