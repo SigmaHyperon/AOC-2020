c1a79cf6fa238b1970856192993de46b4ffe858e
SigmaHyperon <nano_tech@web.de>
2020-12-18 18:24:34 +0100

    solved 17

--- 17/index.js
+++ 17/index.js
@@ -1,9 +1,6 @@
 const fs = require("fs");
 
 const data = fs.readFileSync("input.txt");
-// const data = `.#.
-// ..#
-// ###`
 const values = data.toString().split("\n");
 
 class Spot {
@@ -95,22 +92,99 @@ class Space {
 	occupiedCount() {
 		return this.occupied.length;
 	}
-}
 
-function parseInput(input) {
-	const space = new Space();
-	for(const [xIndex, line] of input.entries()) {
-		for(const [yIndex, block] of [...line].entries()) {
-			if(block === "#") {
-				space.occupied.push(new Spot(xIndex, yIndex, 0));
+	import(input) {
+		for(const [xIndex, line] of input.entries()) {
+			for(const [yIndex, block] of [...line].entries()) {
+				if(block === "#") {
+					this.occupied.push(new Spot(xIndex, yIndex, 0));
+				}
 			}
 		}
 	}
-	return space;
 }
 
-let part1 = parseInput(values);
+console.time("time");
+let part1 = new Space();
+part1.import(values);
 for(let i = 0; i < 6; i++) {
 	part1 = part1.getNextGeneration();
 }
 console.log("part1:", part1.occupiedCount());
+console.timeEnd("time");
+
+class Spot4 extends Spot {
+	constructor(x, y, z, w) {
+		super(x, y, z);
+		this.w = w;
+	}
+
+	equals(spot) {
+		return super.equals(spot) && spot.w === this.w;
+	}
+
+	adjacentCoordinates() {
+		let surrounding = [];
+		for(let x = this.x - 1; x <= this.x + 1; x++) {
+			for(let y = this.y - 1; y <= this.y + 1; y++) {
+				for(let z = this.z - 1; z <= this.z + 1; z++) {
+					for(let w = this.w - 1; w <= this.w + 1; w++) {
+						const nSpot = new Spot4(x, y, z, w);
+						if(!nSpot.equals(this)) {
+							surrounding.push(nSpot);
+						}
+					}
+				}
+			}
+		}
+		return surrounding;
+	}
+}
+
+class Space4 extends Space {
+	getBorders() {
+		const s = super.getBorders();
+		const w = this.occupied.map(v => v.w);
+		s.w = {
+			min: Math.min(...w),
+			max: Math.max(...w)
+		}
+		return s;
+	}
+	getNextGeneration() {
+		const nextSpace = new Space4();
+		const borders = this.getBorders();
+		for(let x = borders.x.min - 1; x <= borders.x.max + 1; x++) {
+			for(let y = borders.y.min - 1; y <= borders.y.max + 1; y++) {
+				for(let z = borders.z.min - 1; z <=borders.z.max + 1; z++) {
+					for(let w = borders.w.min - 1; w <=borders.w.max + 1; w++) {
+						const spot = new Spot4(x, y, z, w);
+						const surrounding = spot.adjacentCoordinates().filter(v => this.isOccupied(v)).length;
+						if((this.isOccupied(spot) && surrounding === 2) || surrounding === 3) {
+							nextSpace.occupied.push(spot);
+						}
+					}
+				}
+			}
+		}
+		return nextSpace;
+	}
+	import(input) {
+		for(const [xIndex, line] of input.entries()) {
+			for(const [yIndex, block] of [...line].entries()) {
+				if(block === "#") {
+					this.occupied.push(new Spot4(xIndex, yIndex, 0, 0));
+				}
+			}
+		}
+	}
+}
+
+console.time("time");
+let part2 = new Space4();
+part2.import(values);
+for(let i = 0; i < 6; i++) {
+	part2 = part2.getNextGeneration();
+}
+console.log("part2:", part2.occupiedCount());
+console.timeEnd("time");
